d4dede1f44ab6a652aa6698437fed4230fb7a173
ok1054689 <228604034@qq.com>
2023-04-09 21:19:46 +0800

    test

--- index.js
+++ index.js
@@ -1,17 +1,23 @@
 // import * as mqtt from "mqtt"  // import everything inside the mqtt module and give it the namespace "mqtt"
 const mqtt = require('mqtt')
 let client = mqtt.connect('mqtt://1.14.96.71') // create a client
+const config = {
+    topic: "self_room_command",
+    type: "relay"
+}
+
+
 // console.log(client);
 client.on('connect', function () {
-    client.subscribe('self_room_command', function (err) {
+    client.subscribe(config.topic, function (err) {
         if (!err) {
-            client.publish('self_room_command', 'init connect mqtt')
+            client.publish(config.topic, 'init connect mqtt')
         }
     })
 })
 
-client.on('self_room_command', function (topic, message) {
+client.on("message", function (topic, message) {
     // message is Buffer
-    console.log(topic.toString(), message.toString())
+    console.log( message.toString())
     // client.end()
 })
