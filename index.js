932f0ae77195d263ee37c6b706f44a89e9a2b1f6
ok1054689 <228604034@qq.com>
2023-04-11 16:58:56 +0800

    test

--- index.js
+++ index.js
@@ -1,44 +1,69 @@
 // import * as mqtt from "mqtt"  // import everything inside the mqtt module and give it the namespace "mqtt"
 const mqtt = require('mqtt')
-let client = mqtt.connect('mqtt://1.14.96.71', {
+const Gpio = require('onoff').Gpio;
+
+const config = {
+    brokerUrl: 'mqtt://1.14.96.71',
+    topic: "device/nanchong_wanda_1",
     username: 'test',
     password: 'test',
-}) // create a client
-const config = {
-    topic: "self_room_command",
-    type: "relay"
 }
+
+let client = mqtt.connect(config.brokerUrl, {
+    username: config.username,
+    password: config.password,
+}) // create a client
+
 
 
 // console.log(client);
 client.on('connect', function () {
-    // client.subscribe(config.topic, function (err) {
-    //     if (!err) {
-    //         client.publish(config.topic, 'init connect mqtt')
-    //     }
-    // })
+    client.subscribe(config.topic, function (err) {
+        if (!err) {
+            client.publish(config.topic, 'init connect mqtt')
+        }
+    })
 
     //请求api这个店里所有的房间。
 
 })
 
 client.on("message", function (topic, message) {
-
+    /**
+     * {
+     *      pin:198,
+     *      type: "gpio",
+     *      action:"off"
+     * }
+     */
     try {
         const msgObj = JSON.parse(message)
-        if (msgObj) {
-            const expireDate = new Date(msgObj.expire)
-            if (expireDate < new Date()) {
-                // 关电
+        let result = null
 
-                // client.publish(config.topic, '关电成功')
-                console.log("已到期，关电成功");
-            } else {
-                // client.publish(config.topic, '未到期')
-                console.log("未到期");
-            }
-
+        /**
+         * 到期时间
+         */
+        // if (msgObj) {
+        //     const expireDate = new Date(msgObj.expire)
+        //     if (expireDate < new Date()) {
+        //         // 关电
+        //         // client.publish(config.topic, '关电成功')
+        //         console.log("已到期，关电成功");
+        //     } else {
+        //         // client.publish(config.topic, '未到期')
+        //         console.log("未到期");
+        //     }
+        // }
+        if (msgObj.type == "gpio" && msgObj.pin && msgObj.action) {
+            const pin = new Gpio(msgObj.pin, 'out');
+            const value = msgObj.action == "off" ? 0 : 1; // 切换 GPIO 值（异或运算）
+            pin.writeSync(value); // 将新值写入 GPIO
+            //后续处理
+            result = pin.readSync()
+            console.log({ ...msgObj, result })
+            client.publish(config.topic + "/state/" + msgObj.pin, JSON.stringify({ result }))
         }
+        
     } catch (e) {
         console.log(topic, e);
         // return false;
@@ -56,4 +81,4 @@ async function doSomething() {
     }
 }
 
-doSomething();
+// doSomething();
