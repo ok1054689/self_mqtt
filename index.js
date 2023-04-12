226970b2c55e96a7ee3546c527e13977ef1ab8bc
ok1054689 <228604034@qq.com>
2023-04-12 19:22:14 +0800

    加入hass

--- index.js
+++ index.js
@@ -10,28 +10,49 @@ const config = {
     username: process.env.MQTT_USERNAME,
     password: process.env.MQTT_PASSWORD,
     beattim: process.env.BEATTIM ?? 1,
+    discovery_prefix: "homeassistant"
 }
+// homeassistant 发现设备格式 https://www.home-assistant.io/integrations/mqtt/#discovery-examples
+// <discovery_prefix>/<component>/[<node_id>/]<object_id>/config 
+//<discovery_prefix>/<component>/<id>/config 
+
+
 
 const devices = {
-    power_1: {
+    "power_1": {
+        name: "卧室灯电源",
         pin: 198,
         type: "relay",
+        component: "switch",
+        mqtt: {
+            "~": `${config.discovery_prefix}/switch/${config.controlDeviceId}/power_1`,
+            unique_id: config.controlDeviceId + "_" + "power_1",
+            cmd_t: "~/set",
+            stat_t: "~/state",
+            schema: "json",
+            payload_on: '{"key": "power_1","on": "on"}',
+            payload_off: '{"key": "power_1"}',
+            state_on: "ON",
+            state_off: "OFF",
+        }
+
+        // HASS_object_id: process.env.CONTROL_DEVICE_ID + "power_1"
     }
 }
 
 const handlers = {
     relay: {
         set: (pin, on) => {
-            on = on == "on" ? 1 : 0
-            const gpio = new Gpio(pin, 'out');
-            gpio.writeSync(on); // 将新值写入 GPIO
-            //返回结果
-            return gpio.readSync()
+            // on = on == "on" ? 1 : 0
+            // const gpio = new Gpio(pin, 'out');
+            // gpio.writeSync(on); // 将新值写入 GPIO
+            // //返回结果
+            // return gpio.readSync()
         },
         get: (pin) => {
-            const gpio = new Gpio(pin);
-            //返回结果
-            return gpio.readSync()
+            // const gpio = new Gpio(pin);
+            // //返回结果
+            // return gpio.readSync()
         }
     }
 }
@@ -47,7 +68,7 @@ let client = mqtt.connect(config.brokerUrl, {
 
 
 client.on('connect', function () {
-    const topic = config.controlDeviceId + "/+/set"
+    const topic = `${config.discovery_prefix}/+/${config.controlDeviceId}/+/set`
     client.subscribe(topic, { qos: 2 }, function (err) {
         if (!err) {
             console.log('init connect mqtt', `subscribe topic: ${topic}`)
@@ -73,13 +94,13 @@ client.on("message", function (topic, message) {
         if (msgObj.key) {
             const device = devices[msgObj.key]
             if (device.type == "relay" && device.pin) {
-                const topic = `${config.controlDeviceId}/${msgObj.key}/state`
+                const topic = `${device.mqtt["~"]}/state`
                 console.log("device", msgObj.on, device);
                 client.publish(
                     topic,
                     JSON.stringify({
-                        device,
-                        state: handlers.relay.set(device.pin, msgObj.on),
+                        // device,
+                        state: handlers.relay.set(device.pin, msgObj.on) ? "ON" : "OFF",
                         topic
                     })
                 )
@@ -116,11 +137,44 @@ setInterval(async () => {
             // console.log("publish", type, pin, mqttTopic, result);
 
         } catch (error) {
-            console.error("client.publish", error, device);
+            console.error("client.publish", error);
         }
     })
 
 
 }, config.beattim * 1000);
+
+
+/**
+ * homeassistant 发现设备格式
+ */
+setInterval(async () => {
+    console.log("mqtt config");
+    const keys = Object.keys(devices);
+    keys.map((key) => {
+        try {
+            const device = devices[key]
+
+            if (device) {
+                // const topic = `${config.controlDeviceId}/${key}/state`
+
+                const topic = `${device.mqtt["~"]}`
+                // const device
+                if (device.type == "relay" && device.pin) {
+                    client.publish(
+                        topic + "/config",
+                        JSON.stringify(device.mqtt)
+                    )
+                }
+            }
+            // console.log("publish", type, pin, mqttTopic, result);
+
+        } catch (error) {
+            console.error("client.publish", error, device);
+        }
+    })
+
+
+}, 10000);
 
 
