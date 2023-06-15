bcd16f9de3dfbe040d48a7abca0dc91f2edb454e
ok1054689 <228604034@qq.com>
2023-06-15 19:33:26 +0800

    1

--- index.js
+++ index.js
@@ -106,16 +106,15 @@ client.on("message", function (topic, message) {
         }
 
     } catch (e) {
-        // console.log(topic, e);
-        client.publish(
-            "debug",
-            JSON.stringify({
-                // device,
-                topic,
-                e
-
-            })
-        )
+        console.log(topic, e);
+        // client.publish(
+        //     "debug",
+        //     JSON.stringify({
+        //         // device,
+        //         topic,
+        //         e
+        //     })
+        // )
         // return false;
     }
     // client.end()
